8bcb0fabd4ab80c30895087dbcb7801700969a2d
Hritam Jaiwal <43675990+Hritam9@users.noreply.github.com>
2026-07-19 18:26:03 +0530

    Create experiments.py

---
+++ mlops/experiments.py
@@ -0,0 +1,27 @@
+DEFAULT_EXPERIMENT = {
+
+    "embedding_model":
+
+        "bge-small",
+
+    "chunk_size":
+
+        500,
+
+    "chunk_overlap":
+
+        50,
+
+    "retriever":
+
+        "Hybrid",
+
+    "reranker":
+
+        "CrossEncoder",
+
+    "top_k":
+
+        5,
+
+}
