eb90e2d0de84325b471588775eb7c4768e745e32
Hritam Jaiwal <43675990+Hritam9@users.noreply.github.com>
2026-07-19 18:25:28 +0530

    Create registry.py

---
+++ mlops/registry.py
@@ -0,0 +1,17 @@
+class ModelRegistry:
+
+    EMBEDDINGS = {
+
+        "bge-small": "BAAI/bge-small-en-v1.5",
+
+        "e5-small": "intfloat/e5-small-v2",
+
+    }
+
+    LLM = {
+
+        "groq":
+
+        "llama-3.1-8b-instant"
+
+    }
